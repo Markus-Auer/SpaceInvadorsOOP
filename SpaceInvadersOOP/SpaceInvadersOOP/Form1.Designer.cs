ed40a0a0e0073bf48ce8d79c864598e0ca431c44
Markus-Auer <45470745+Markus-Auer@users.noreply.github.com>
2020-01-16 12:34:28 +0100

    Add files via upload

---
+++ SpaceInvadersOOP/SpaceInvadersOOP/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace SpaceInvadersOOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pBplayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pBplayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // pBplayer
+            // 
+            this.pBplayer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pBplayer.Location = new System.Drawing.Point(356, 357);
+            this.pBplayer.Name = "pBplayer";
+            this.pBplayer.Size = new System.Drawing.Size(22, 24);
+            this.pBplayer.TabIndex = 0;
+            this.pBplayer.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pBplayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBplayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pBplayer;
+    }
+}
+
---
+++ SpaceInvadersOOP/SpaceInvadersOOP/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvadersOOP
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        GameElements player1 = new GameElements();
+        bool leftout = false;
+        bool rightout;
+
+        public void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (!rightout)
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    pBplayer.Left += player1.move(player1.speed_ = 4);
+                }
+            }
+            if (!leftout)
+            {
+                if (e.KeyCode == Keys.Left)
+                {
+                    pBplayer.Left -= player1.move(player1.speed_ = 4);
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (pBplayer.Bounds.IntersectsWith(this.Bounds))
+            {
+             
+
+            }
+
+           /* int x = pBplayer.Location.X;
+            int y = pBplayer.Location.Y;
+
+            if (x <= 0 )
+            {
+                leftout = true;
+            }else
+            {
+                leftout = false;
+            }
+            if(x >= this.Width)
+            {
+                rightout = true;
+            }
+            else
+            {
+                rightout = false;
+            }
+            */ 
+
+        }
+    }
+}
